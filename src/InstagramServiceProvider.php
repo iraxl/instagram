de348fb338e2d75db93978f81683f5514d756d90
EdenPatch Production <venky@edenpatch.com>
2015-12-03 00:50:38 +0000

    itest

--- src/InstagramServiceProvider.php
+++ src/InstagramServiceProvider.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Vinkla\Instagram;
+namespace Iraxl\Instagram;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Application as LaravelApplication;
